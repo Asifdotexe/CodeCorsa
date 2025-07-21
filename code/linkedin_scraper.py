74c8770f5e099b4d71e3e37e59756c75e96fa347
Asif Sayyed <asifdotexe@gmail.com>
2025-07-21 17:00:59 +0530

    #1 created url generator

--- code/linkedin_scraper.py
+++ code/linkedin_scraper.py
@@ -2,4 +2,13 @@ import requests
 from bs4 import BeautifulSoup
 
 
-URL = "https://www.linkedin.com/jobs/search/?keywords=Python%20Developer&location=United%20States"
+def create_linkedin_search_url(keywords: str, location: str) -> str:
+    """
+    Create a LinkedIn job search URL based on keywords and location.
+
+    :param keywords: The job keywords to search for.
+    :param location: The location to search in.
+    :return: A formatted LinkedIn job search URL.
+    """
+    url = f"https://www.linkedin.com/jobs/search/?keywords={keywords}&location={location}"
+    return url
